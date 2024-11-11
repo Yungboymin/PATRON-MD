12523cf9b83b8c3fdf5598c8a3d3286a1ca14c52
Yung <154564533+Yungboymin@users.noreply.github.com>
2024-11-11 01:58:44 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const chalk = require("chalk")
 const fs = require("fs")
 //auto presence update
-global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoTyping = true //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
 global.autoblockmorroco = true //auto block 212 (true to on, false to off)
 global.autokickmorroco = true //auto kick 212 (true to on, false to off) 
-global.antispam = false//auto kick spammer (true to on, false to off)
+global.antispam = true//auto kick spammer (true to on, false to off)
 //===============SETTING MENU==================\\
 var _0x6e9f5f = _0x440c;
 function _0x440c(_0xff7e8a, _0x55b825) {
@@ -63,10 +63,10 @@ function _0x8e09() {
     }
   }
 }(_0x8e09, 105820), global[_0x1bcf00(146)] = _0x1bcf00(131), global[_0x1bcf00(137)] = _0x1bcf00(150), global[_0x1bcf00(127)] = "2348133729715", global.ownername = _0x1bcf00(138), global[_0x1bcf00(149)] = [_0x1bcf00(129)], global.ownerweb = "https://whatsapp.com/channel/0029Val0s0rIt5rsIDPCoD2q", global[_0x1bcf00(153)] = _0x1bcf00(147), global[_0x1bcf00(152)] = _0x1bcf00(147), global.saluran = _0x1bcf00(147), global.themeemoji = "🚹", global.wm = "ᴘᴀᴛʀᴏɴ-ᴍᴅ", global[_0x1bcf00(145)] = _0x1bcf00(136), global[_0x1bcf00(132)] = _0x1bcf00(128), global[_0x1bcf00(141)] = _0x1bcf00(134))
-global.creator = "2348133729715@s.whatsapp.net"
+global.creator = "2349016868461@s.whatsapp.net"
 global.bankname = "MONIEPOINT"
 global.banknumber = "5518447058"
-global.bankowner = "FADARE"
+global.bankowner = "𝐄𝐓𝐄𝐄𝐑𝐍𝐋𝐗𝐙"
 //======================== CPANEL COMMAND ===========================\\
 global.domain = '-' // Fill in your domain, don't put a / at the end of the link
 global.apikey = '-' // Fill Apikey
@@ -190,7 +190,7 @@ global.rpg = {
 }
 
 //new
-global.prefix = ['!','.','#','&']
+global.prefix = ['!','.']
 global.sessionName = 'session'
 global.hituet = 0
 //media target
